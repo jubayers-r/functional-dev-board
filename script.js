f34028a54e8099e4717ab3b854c8a636ff4e9235
Jubayer Shikder <validjubayer@gmail.com>
2025-03-02 22:49:58 +0600

    completed js and qna blog

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 // theme btn
 
 document.getElementById("theme-btn").addEventListener("click", function () {
-    const body = document.body;
-    const colors = [
-        "rgb(155, 95, 224)", "rgb(22, 164, 216)", "rgb(96, 219, 232)", "rgb(139, 211, 70)", "rgb(239, 223, 72)",
-        "rgb(249, 165, 44)", "rgb(214, 78, 18)"
-    ];
+  const body = document.body;
+  const colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 128, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(128, 0, 255)",
+  ];
 
-    let currentColor = body.style.backgroundColor;
+  let currentColor = body.style.backgroundColor;
 
-   if (body.classList.contains("bg-[#f4f7ff]")) {
+  if (body.classList.contains("bg-[#f4f7ff]")) {
     body.classList.remove("bg-[#f4f7ff]");
     body.style.backgroundColor = colors[0];
-   } else {
+  } else {
     let colorIdx = colors.indexOf(currentColor);
-    if(currentColor === -1 || colorIdx === colors.length - 1){
-        body.classList.add("bg-[#f4f7ff]");
-        body.style.backgroundColor = "";
+    if (currentColor === -1 || colorIdx === colors.length - 1) {
+      body.classList.add("bg-[#f4f7ff]");
+      body.style.backgroundColor = "";
     } else {
-        body.style.backgroundColor = colors[colorIdx + 1]
+      body.style.backgroundColor = colors[colorIdx + 1];
     }
-   }
+  }
 });
 
 // discover click
 
 const discoverBtn = document.getElementById("discover");
 
-discoverBtn.onclick = function(){
-    window.location.href = "./blog.html";
-}
-
+discoverBtn.onclick = function () {
+  window.location.href = "./blog.html";
+};
 
 //btns
 const task = document.getElementById("task");
 const taskDone = document.getElementById("task-done");
-const notifications = document.getElementById('notifications');
+const notifications = document.getElementById("notifications");
 const task1 = document.getElementById("task1").innerText;
 const task2 = document.getElementById("task2").innerText;
 const task3 = document.getElementById("task3").innerText;
@@ -43,27 +47,26 @@ const task4 = document.getElementById("task4").innerText;
 const task5 = document.getElementById("task5").innerText;
 
 // live time
-let timeNow = '';
+let timeNow = "";
 function updateClock() {
-    const now = new Date();
-    let hours = now.getHours();
-    const meridiem = hours < 12 ? 'AM' : 'PM';
-    hours = hours % 12 || 12;
-    hours = hours.toString().padStart(2, 0);
-    const mins = now.getMinutes().toString().padStart(2, 0);
-    const secs = now.getSeconds().toString().padStart(2, 0);
+  const now = new Date();
+  let hours = now.getHours();
+  const meridiem = hours < 12 ? "AM" : "PM";
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2, 0);
+  const mins = now.getMinutes().toString().padStart(2, 0);
+  const secs = now.getSeconds().toString().padStart(2, 0);
 
-    timeNow = `${hours}:${mins}:${secs} ${meridiem}`;
-    return timeNow;
+  timeNow = `${hours}:${mins}:${secs} ${meridiem}`;
+  return timeNow;
 }
 
 setInterval(updateClock, 1000);
 updateClock();
 
-
 document.getElementById("btn1").addEventListener("click", function () {
-const btn1 = document.getElementById("btn1");
-if(btn1.classList.contains("btn-primary")){
+  const btn1 = document.getElementById("btn1");
+  if (btn1.classList.contains("btn-primary")) {
     btn1.classList.remove("btn-primary");
     btn1.classList.remove("bg-[#3752FD]");
     btn1.classList.add("text-white");
@@ -76,11 +79,13 @@ if(btn1.classList.contains("btn-primary")){
               You have Completed The Task ${task1} at ${timeNow}
             </p>
           </div>`;
-}
-})
+    alert("Board updated successfully");
+  }
+  checkAllTasksCompleted();
+});
 document.getElementById("btn2").addEventListener("click", function () {
-const btn1 = document.getElementById("btn1");
-if(btn2.classList.contains("btn-primary")){
+  const btn1 = document.getElementById("btn1");
+  if (btn2.classList.contains("btn-primary")) {
     btn2.classList.remove("btn-primary");
     btn2.classList.remove("bg-[#3752FD]");
     btn2.classList.add("text-white");
@@ -92,11 +97,13 @@ if(btn2.classList.contains("btn-primary")){
               You have Completed The Task ${task2} at ${timeNow}
             </p>
           </div>`;
-}
-})
+    alert("Board updated successfully");
+  }
+  checkAllTasksCompleted();
+});
 document.getElementById("btn3").addEventListener("click", function () {
-const btn3 = document.getElementById("btn3");
-if(btn3.classList.contains("btn-primary")){
+  const btn3 = document.getElementById("btn3");
+  if (btn3.classList.contains("btn-primary")) {
     btn3.classList.remove("btn-primary");
     btn3.classList.remove("bg-[#3752FD]");
     btn3.classList.add("text-white");
@@ -109,11 +116,13 @@ if(btn3.classList.contains("btn-primary")){
               You have Completed The Task ${task3} at ${timeNow}
             </p>
           </div>`;
-}
-})
+    alert("Board updated successfully");
+  }
+  checkAllTasksCompleted();
+});
 document.getElementById("btn4").addEventListener("click", function () {
-const btn4 = document.getElementById("btn4");
-if(btn4.classList.contains("btn-primary")){
+  const btn4 = document.getElementById("btn4");
+  if (btn4.classList.contains("btn-primary")) {
     btn4.classList.remove("btn-primary");
     btn4.classList.remove("bg-[#3752FD]");
     btn4.classList.add("text-white");
@@ -125,11 +134,13 @@ if(btn4.classList.contains("btn-primary")){
               You have Completed The Task ${task4} at ${timeNow}
             </p>
           </div>`;
-}
-})
+    alert("Board updated successfully");
+  }
+  checkAllTasksCompleted();
+});
 document.getElementById("btn5").addEventListener("click", function () {
-const btn5 = document.getElementById("btn5");
-if(btn5.classList.contains("btn-primary")){
+  const btn5 = document.getElementById("btn5");
+  if (btn5.classList.contains("btn-primary")) {
     btn5.classList.remove("btn-primary");
     btn5.classList.remove("bg-[#3752FD]");
     btn5.classList.add("text-white");
@@ -141,30 +152,42 @@ if(btn5.classList.contains("btn-primary")){
               You have Completed The Task ${task5} at ${timeNow}
             </p>
           </div>`;
-}
-})
+    alert("Board updated successfully");
+  }
+  checkAllTasksCompleted();
+});
 
+// all tasked completed alert
+function checkAllTasksCompleted() {
+  const buttons = [
+    document.getElementById("btn1"),
+    document.getElementById("btn2"),
+    document.getElementById("btn3"),
+    document.getElementById("btn4"),
+    document.getElementById("btn5"),
+  ];
+
+  const allCompleted = buttons.every(
+    (btn) => !btn.classList.contains("btn-primary")
+  );
+
+  if (allCompleted) {
+    alert("Congrats!!! You have completed all the current task");
+  }
+}
 
 // clear log
 
 document.getElementById("clear").addEventListener("click", function () {
-    notifications.innerHTML = "";
-})
+  notifications.innerHTML = "";
+});
 
 // live date
 const today = new Date();
-const day = today.toLocaleString('en-US', { weekday: 'short' });
-const month = today.toLocaleString('en-US', { month: 'short' });
+const day = today.toLocaleString("en-US", { weekday: "short" });
+const month = today.toLocaleString("en-US", { month: "short" });
 const date = today.getDate();
 const year = today.getFullYear();
 
 const fullDate = document.getElementById("date");
 fullDate.innerHTML = `${day} , <br> <span class="font-bold"> ${month} ${date} ${year}</span>`;
-
-// back to desk button
-
-const backBtn = document.getElementById("back-btn");
-
-backBtn.onclick = function(){
-    window.location.href = "./index.html";
-}
